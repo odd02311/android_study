fd383dae97d6b76c9ab9e8302e52e6f1647c5c64
Delavuida <jaju1995@naver.com>
2024-12-06 16:37:23 +0900

    #8-switch-app
    단위 변환 앱 기능 구현

--- chapter3/app/src/main/java/com/example/part1/chapter3/MainActivity.kt
+++ chapter3/app/src/main/java/com/example/part1/chapter3/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.part1.chapter3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import com.example.part1.chapter3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,8 +19,39 @@ class MainActivity : AppCompatActivity() {
         val outputTextView = binding.outputTextView
         val outputUnitTextView = binding.outputUnitTextView
         val inputEditText = binding.inputEditText
-        val inputUnitView = binding.inputUnitText
+        val inputUnitTextView = binding.inputUnitText
+        val swapImageButton = binding.swapImageButton
 
+        var inputNumber : Int = 0
+        var cmToM = true
+
+        inputEditText.addTextChangedListener{ text ->
+                inputNumber = if(text.isNullOrEmpty()){
+                    0
+                } else {
+                    text.toString().toInt()
+                }
+            
+            // 자동 단위 변환 식
+            if(cmToM){
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
         
+        // 버튼을 눌러서 단위 변환 식
+        swapImageButton.setOnClickListener {
+            cmToM = cmToM.not()
+            if(cmToM){
+                inputUnitTextView.text = "cm"
+                outputUnitTextView.text = "m"
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                inputUnitTextView.text = "m"
+                outputUnitTextView.text = "cm"
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
     }
 }
