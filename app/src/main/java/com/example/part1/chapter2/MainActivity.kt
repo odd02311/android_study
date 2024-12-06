6bee760ec850d36a4864887ae488aa40855654f7
Delavuida <jaju1995@naver.com>
2024-12-06 11:57:05 +0900

    #3 유저 입력값 받아오기
    id 지정 이후 text/button 선언
    reset/plus 버튼 클릭 로그로 확인

--- app/src/main/java/com/example/part1/chapter2/MainActivity.kt
+++ app/src/main/java/com/example/part1/chapter2/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.part1.chapter2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 화면 요소 가져오기
+        val numberTextView = findViewById<TextView>(R.id.numberTextView)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+        val plusButton = findViewById<Button>(R.id.plusButton)
+
+        // 클릭 체킹
+        // setOnClickListener = 클릭을 했을때 액션
+        resetButton.setOnClickListener {
+            // 확인용 로그
+            Log.d("onclick", "리셋 버튼이 클릭 되었습니다.")
+        }
+
+        plusButton.setOnClickListener {
+            Log.i("onclick", "플러스 버튼이 클릭 되었습니다.")
+        }
     }
 }
