6d17380dade29150060c4ae71ded80062a3cf449
garry02311 <jaju1995@naver.com>
2024-12-16 11:22:43 +0900

    #39-constraint-card
    constraintLayout 속성을 이용해 카드 구현

---
+++ compose_study_2/Constraint_Card/app/src/main/java/com/example/part3/constraintcard/MainActivity.kt
@@ -0,0 +1,162 @@
+package com.example.part3.constraintcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import coil.compose.AsyncImage
+import com.example.part3.constraintcard.ui.theme.ConstraintCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ConstraintCardTheme {
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(modifier = Modifier.fillMaxSize()
+                        .padding(WindowInsets.statusBars.asPaddingValues())
+                    ) {
+                        CardEx(cardData)
+                        CardEx(cardData)
+                        CardEx(cardData)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CardEx(cardData: CardData){
+    val placeHolderColor = Color(0x33000000)
+
+    Card(
+        elevation = CardDefaults.cardElevation(8.dp),
+        modifier = Modifier.padding(4.dp)
+            .fillMaxWidth(),
+    ){
+        // 1. 아래의 Row 레이아웃을 ConstraintLayout으로 바꾸기
+        ConstraintLayout(modifier = Modifier.fillMaxWidth()
+            .wrapContentHeight()
+            .padding(8.dp)
+        ){
+            val (profileImage, author, description) = createRefs()
+
+            AsyncImage(
+                model = cardData.imageUri,
+                contentDescription = cardData.imageDescription, // 오타 수정
+                contentScale = ContentScale.Crop,
+                placeholder = ColorPainter(color = placeHolderColor), // 속성 이름 변경
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(40.dp)
+                    .constrainAs(profileImage) {
+                        centerVerticallyTo(parent)
+                        start.linkTo(parent.start, 8.dp)
+//                        linkTo(parent.top, parent.bottom)
+//                        top.linkTo(parent.top)
+//                        bottom.linkTo(parent.bottom)
+
+                    }
+            )
+
+            Text(
+                text = cardData.author,
+                modifier = Modifier.constrainAs(author) {
+                    linkTo(
+                        profileImage.end,
+                        parent.end,
+                        startMargin = 8.dp,
+                        endMargin = 8.dp
+                    )
+                    width = Dimension.fillToConstraints
+                }
+            )
+
+            // Description Text
+            Text(
+                text = cardData.description,
+                modifier = Modifier.constrainAs(description) {
+                    linkTo(
+                        profileImage.end,
+                        parent.end,
+                        startMargin = 8.dp,
+                        endMargin = 8.dp
+                    )
+                    width = Dimension.fillToConstraints
+                }
+            )
+
+            val chain = createVerticalChain(
+                author,
+                description,
+                chainStyle = ChainStyle.Packed
+            )
+
+            constrain(chain){
+                top.linkTo(parent.top, margin = 8.dp)
+                bottom.linkTo(parent.bottom, margin = 8.dp)
+            }
+
+        }
+
+//        Row(
+//            verticalAlignment = Alignment.CenterVertically,
+//            modifier = Modifier.padding(8.dp),
+//        ){
+//            AsyncImage(
+//                model = cardData.imageUri,
+//                contentDescription = cardData.imageDescription, // 오타 수정
+//                contentScale = ContentScale.Crop,
+//                placeholder = ColorPainter(color = placeHolderColor), // 속성 이름 변경
+//                modifier = Modifier
+//                    .clip(CircleShape)
+//                    .size(40.dp)
+//            )
+//            Spacer(modifier = Modifier.size(8.dp))
+//            Column {
+//                Text(text = cardData.author)
+//                Text(text = cardData.description)
+//            }
+//        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ConstraintCardTheme {
+        CardEx(cardData)
+    }
+}
+
+data class CardData(
+    val imageUri: String,
+    val imageDescription: String,
+    val author: String,
+    val description: String,
+)
+
+val cardData = CardData(
+    imageUri = "https://raw.githubusercontent.com/Fastcampus-Android-Lecture-Project-2023/part4-chapter3/main/part4-chapter3-10/app/src/main/res/drawable-xhdpi/wall.jpg",
+    imageDescription = "엔텔로프 캐년",
+    author = "Dalinaum",
+    description = "엔텔로프 캐년은 죽기 전에 꼭 봐야할 절경으로 소개되었습니다."
+)
